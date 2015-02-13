5cf87c67f63f1c123c91cb9209dd259087f37411
Blackwolf <amin.sdouga@epita.fr>
2015-02-13 21:22:03 +0400

    (e)mergency lol

---
+++ Bignum.cpp
@@ -0,0 +1,30 @@
+#include "Bignum.h"
+/*
+Bignum operator+(const Bignum& a, const Bignum& b)
+{
+    Bignum copie(a);
+    a += b;
+    return a;
+}
+
+Bignum operator-(const Bignum& a, const Bignum& b)
+{
+    Bignum copie(a);
+    a -= b;
+    return a;
+}
+
+Bignum operator*(const Bignum& a, const Bignum& b)
+{
+    Bignum copie(a);
+    a *= b;
+    return a;
+}
+
+Bignum operator/(const Bignum& a, const Bignum& b)
+{
+    Bignum copie(a);
+    a /= b;
+    return a;
+}
+*/
