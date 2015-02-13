46165f6fedc9e970bb408db26bf977168676aa3f
Phacocherman <chomin_a@epita.fr>
2015-02-13 21:27:16 +0100

    operator+= with Bignum advanced

--- Bignum.cpp
+++ Bignum.cpp
@@ -1,4 +1,4 @@
-/* Bignum.cpp
+/** Bignum.cpp
  * by Phacocherman
  * 02/13/2015 | 02/13/2015
  * Defines functions for Bignum class.
@@ -17,9 +17,23 @@ Bignum operator+()
 
 }
 
-void Bignum::operator+=(const Bignum& added)
+bool Bignum::operator+=(const Bignum& added)
 {
     unsigned int i = 0;
-    unsigned char retenue;
+    unsigned char retenue = 0;
+    unsigned char value;
 
+    while (i < A_Signed)
+    {
+        value = A_Bignum[i] + added[i];
+        A_Bignum[i] = (value % 10) + retenue;
+        retenue = value / 10;
+    }
+
+    if (i < Max_Bignum - 1)
+        A_Bignum[i+1] = retenue;
+    else if (retenue > 0)
+        return false; /// le Bignum a depasse Max Bignum
+
+    return true;
 }
