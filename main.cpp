f09b4e380f3c86e837164f307cc9e88ced840e30
Blackwolf <amin.sdouga@epita.fr>
2015-07-17 13:19:17 +0200

    correction de plusieurs bug
    ce projet est finalement très sale et bâclé et je soupçonne une fuite de
    mémoire énorme due a la multiplication

--- main.cpp
+++ main.cpp
@@ -5,11 +5,10 @@ using namespace std;
 int main()
 {
     unsigned long long int i(0);
-    Bignum prout(50);
+    Bignum prout(0);
 
-    display(prout);
-    cin >> prout;
-    display(prout);
+    for(; prout < 50; ++prout)
+        cout << "p " << prout << endl << "pdiv2 " << (prout / 2) << endl;
 
     return 0;
 }
