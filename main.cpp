fc55fdbc2e0d1e7a6d33ec49abd3f44ea1f3319f
Blackwolf <amin.sdouga@epita.fr>
2015-07-17 16:26:48 +0200

    correction de la fuite mémoire
    j'ai eu une fuite de 1GO pour m'en rendre compte

--- main.cpp
+++ main.cpp
@@ -4,11 +4,5 @@ using namespace std;
 
 int main()
 {
-    unsigned long long int i(0);
-    Bignum prout(0);
-
-    for(; prout < 50; ++prout)
-        cout << "p " << prout << endl << "pdiv2 " << (prout / 2) << endl;
-
     return 0;
 }
